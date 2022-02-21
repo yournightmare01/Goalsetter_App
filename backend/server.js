cff689cfd4199ac2e64ef762a33f9e6a685713bd
yournightmare01 <zlatkor1001@gmail.com>
2022-02-21 18:41:21 +0100

    cors added

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,20 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: ['https://mernappzlatko.herokuapp.com/', 'http://localhost:3000'],
+  })
+);
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
